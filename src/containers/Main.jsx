52ad9fc1c3d103e3698c26d980a878cc78ab179a
Sasha192 <sasha192.bunin@gmail.com>
2022-10-08 15:31:50 +0300

    test work

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,12 +1,12 @@
 import './Main.scss'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
 export default function Main({curencies}) {
 
 
-    const currencies1 = [
+    const currenciesSelect1 = [
         {
             value: curencies['USD'],
             label: '$ - USD',
@@ -20,7 +20,7 @@ export default function Main({curencies}) {
             label: '₴ - UAH',
         }
     ];
-    const currencies2 = [
+    const currenciesSelect2 = [
         {
             value: curencies['USD'],
             label: '$ - USD',
@@ -36,78 +36,76 @@ export default function Main({curencies}) {
     ];
 
 
-    const [currency1, setCurrency1] = useState('EUR');
-    const [currency2, setCurrency2] = useState('USD');
-    const [cache, setCache] = useState(0)
-    const [cache2, setCache2] = useState(0)
+    const [currency1, setCurrency1] = useState('USD');
+    const [currency2, setCurrency2] = useState("EUR");
+    const [inputValue1, setInputValue1] = useState(0)
+    const [inputValue2, setInputValue2] = useState(0)
 
     const handleChange1 = (event) => {
         setCurrency1(event.target.value);
-        console.log(currency1)
     };
     const handleChange2 = (event) => {
         setCurrency2(event.target.value);
-        console.log(currency2)
-
     };
 
-    const handleInputMath1 = (e) => {
-        setCache2(e.target.value * currency1 / currency2)
-        setCache(0)
+    const handleInputCalculations1 = (e) => {
+        setInputValue2(e.target.value * currency1 / currency2)
+        setInputValue1(0)
     }
 
-    const handleInputMath2 = (e) => {
-        setCache(e.target.value * currency2 / currency1)
-        setCache2(0)
+    const handleInputCalculations2 = (e) => {
+        setInputValue1(e.target.value * currency2 / currency1)
+        setInputValue2(0)
     }
+
 
     return (
         <div className="MainWrapper">
-            <div>
-                <TextField
-                    id="outlined-basic"
-                    label="Enter first value"
-                    variant="outlined"
-                    onChange={handleInputMath1}
-                    value={cache ? cache.toFixed(2) : null}
-                />
-                <TextField
-                    id="outlined-select-currency1"
-                    select
-                    label="Select"
-                    value={currency1}
-                    onChange={handleChange1}
-                    helperText="Please select your currency"
-                >
-                    {currencies1.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-            </div>
-            <div>
-                <TextField
-                    id="outlined-basic"
-                    label="Get here result"
-                    variant="outlined"
-                    value={cache2 ? cache2.toFixed(2) : null}
-                    onChange={handleInputMath2}
-                />
-                <TextField
-                    id="outlined-select-currency2"
-                    select
-                    label="Select"
-                    value={currency2}
-                    onChange={handleChange2}
-                    helperText="Please select your currency"
-                >
-                    {currencies2.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
+            <div className='content'>
+                <div className='input-block1'>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        onChange={handleInputCalculations1}
+                        value={inputValue1 ? inputValue1.toFixed(2) : null}
+                    />
+                    <TextField
+                        id="outlined-select-currency1"
+                        select
+                        label="Select"
+                        value={currency1}
+                        onChange={handleChange1}
+                        helperText="Please select your currency"
+                    >
+                        {currenciesSelect1.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </div>
+                <div className='input-block2'>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        value={inputValue2 ? inputValue2.toFixed(2) : null}
+                        onChange={handleInputCalculations2}
+                    />
+                    <TextField
+                        id="outlined-select-currency2"
+                        select
+                        label="Select"
+                        value={currency2}
+                        onChange={handleChange2}
+                        helperText="Please select your currency"
+                    >
+                        {currenciesSelect2.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </div>
             </div>
         </div>
     )
